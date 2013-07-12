a206d0f72d25d9ca8f46c12bf1cd4ea5f669c48b
Dmitry Markushevich <dmitry@atimi.com>
2013-07-11 20:00:57 -0700

    working version of the pemutation generator

--- src/maxsubsetsum_app.erl
+++ src/maxsubsetsum_app.erl
@@ -3,7 +3,7 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1, simpleCombo/3]).
+-export([start/2, stop/1, simpleCombo/2]).
 
 %% ===================================================================
 %% Application callbacks
@@ -15,25 +15,20 @@ start(_StartType, _StartArgs) ->
 stop(_State) ->
 	ok.
 
+% credit: http://stackoverflow.com/questions/1459152/erlang-listsindex-of-function
+index_of(Item, List) -> index_of(Item, List, 1).
+index_of(_, [], _)  -> not_found;
+index_of(Item, [Item|_], Index) -> Index;
+index_of(Item, [_|Tl], Index) -> index_of(Item, Tl, Index+1).
 
-% perms(L)  -> [[H|T] || H <- L, T <- perms(L--[H])].
-% perms([]) -> [[]];
-% 
-% L in list
-% Result temporary result
-% r - size of the combo
-simpleCombo(_, Result, R) when length(Result) == R ->
-	io:format("##", []), erlang:display(lists:reverse(Result)), io:format("~n"),
-	lists:reverse(Result);
 
-simpleCombo(L, Result, R) when length(L) < R ->
-	io:format("2*~n", []),
-	lists:reverse(Result);
+% generate a list of permutations of size R from list L
+simpleCombo(_,R) when R == 0 ->
+	io:format("*2~n"),
+	[[]];
 
-simpleCombo(L, Result, R) ->
-	io:format("1*", []), erlang:display(L), io:format("##"), erlang:display(lists:reverse(Result)), io:format("~n"),
-	[_|T] = L,
-	[[simpleCombo(T,[X|Result],R) || X <- L]].
+simpleCombo(L,R) ->
+	[[X|T] || X <- L, T<-simpleCombo(lists:nthtail(index_of(X,L),L),R-1)].
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
